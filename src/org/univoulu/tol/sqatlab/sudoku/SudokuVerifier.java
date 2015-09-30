47ab1c34fddf3dee97ecce1a2d4f47d2bfe2b9fe
Itir Karac <karacitir@gmail.com>
2015-09-30 23:28:24 +0300

    Update SudokuVerifier.java

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,7 +3,7 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
+		// returns 0 if the candidate solution is correct
 		return 0;
 	}
 }
